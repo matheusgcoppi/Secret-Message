ec81203162b0a6a37c1be15adb3ad2dd86798347
Matheus Coppi <matheuscoppi22@gmail.com>
2023-03-07 16:40:44 -0300

    controller update

---
+++ api/prisma/migrations/20230307192259_new/migration.sql
@@ -0,0 +1,36 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- CreateTable
+CREATE TABLE [dbo].[UserInfo] (
+    [id] INT NOT NULL IDENTITY(1,1),
+    [name] NVARCHAR(1000) NOT NULL,
+    [pin] NVARCHAR(1000) NOT NULL,
+    [email] NVARCHAR(1000),
+    [pintwo] NVARCHAR(1000) NOT NULL,
+    [text] TEXT,
+    CONSTRAINT [UserInfo_pkey] PRIMARY KEY CLUSTERED ([id]),
+    CONSTRAINT [UserInfo_pin_key] UNIQUE NONCLUSTERED ([pin])
+);
+
+-- CreateTable
+CREATE TABLE [dbo].[Text] (
+    [id] INT NOT NULL IDENTITY(1,1),
+    [text] TEXT NOT NULL,
+    [pin] NVARCHAR(1000) NOT NULL,
+    CONSTRAINT [Text_pkey] PRIMARY KEY CLUSTERED ([id])
+);
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
